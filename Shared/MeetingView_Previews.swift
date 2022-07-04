03120b20ec1ba9d5ed68526beda2722f2c733ead
Terrence Liu <tliu29@calpoly.edu>
2022-07-03 18:42:09 -0700

    Initial commit

--- Shared/MeetingView_Previews.swift
+++ Shared/MeetingView_Previews.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MeetingView_Previews: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
@@ -16,6 +16,7 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MeetingView_Previews()
+        
     }
 }
